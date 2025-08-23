639c8b402c38f830b9dbbc91ecb125ee129679f7
Ramya <ramya.bala221190@gmail.com>
2025-08-23 07:47:47 +0530

    first commit

---
+++ public/ngx-env-temp.js
@@ -0,0 +1,5 @@
+globalThis._NGX_ENV_ = {
+    NG_APP_ENV: "${NG_APP_ENV}",
+    NG_APP_VAR1:"${NG_APP_VAR1}",
+    NG_APP_VAR2:"${NG_APP_VAR2}"
+  };
---
+++ src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    "NG_APP_ENV": import.meta.env.NG_APP_ENV,
+  "NG_APP_VAR1": import.meta.env.NG_APP_VAR1,
+  "NG_APP_VAR2": import.meta.env.NG_APP_VAR2
+};
