0fc0037b1f7e0d0e6c303af8506e23cc6f3d7af3
Ramya <ramya.bala221190@gmail.com>
2025-08-25 16:26:02 +0530

    installed node types for process.env

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -3,3 +3,10 @@ export const environment = {
   "NG_APP_VAR1": import.meta.env.NG_APP_VAR1,
   "NG_APP_VAR2": import.meta.env.NG_APP_VAR2
 };
+
+// export const environment = {
+//   "NG_APP_ENV": process.env.NG_APP_ENV,
+// "NG_APP_VAR1": process.env.NG_APP_VAR1,
+// "NG_APP_VAR2": process.env.NG_APP_VAR2
+// };
+
